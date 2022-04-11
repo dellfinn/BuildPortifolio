db09524ae6bafa4bff92d355ed8bb011b211af35
Italo Ruan <paradoxonitalo@gmail.com>
2022-04-10 23:36:46 -0300

    Alinhamentro entre em contato

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/favicon-16x16.png": "acf205bafdc77686b59879953e9f1778",
 "icons/favicon-32x32.png": "455995ee6abc41e5ed16c0b54d10a56a",
 "icons/favicon.ico": "ef218c0984d9228cde4e410012e906ff",
-"index.html": "5bbe9278f32b52bc187f848ee2166a74",
-"/": "5bbe9278f32b52bc187f848ee2166a74",
-"main.dart.js": "43974a987d621a60b475edc7840c2b29",
+"index.html": "393308deeecfa9fadbc82d51256a4ea1",
+"/": "393308deeecfa9fadbc82d51256a4ea1",
+"main.dart.js": "99f8ea581262c90a398498de923302b2",
 "manifest.json": "641073f1a61f23a81a1f2465a079ab5c",
 "version.json": "15c24dbfa284180df22c80d35eb89238"
 };
